25812a6183d30d086bf5e1fd26ec7acadb6c9caa
Shaileshnparkhi <shaileshnparkhi@gmail.com>
2018-06-22 23:42:57 +0530

    header and subheader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Marvellous InfoSystem';
+  subtitle ='Educating for better tomorrow'
 }
